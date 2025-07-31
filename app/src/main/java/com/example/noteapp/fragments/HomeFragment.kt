f5ee50570dbe6b878829ea8a45a79669f4093f62
organizedanvrchy <vimalramnarain@gmail.com>
2025-07-30 21:43:43 -0400

    Update Home Fragment files

--- app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/HomeFragment.kt
@@ -5,29 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.navigation.findNavController
+import com.example.noteapp.MainActivity
 import com.example.noteapp.R
+import com.example.noteapp.adapter.NoteAdapter
+import com.example.noteapp.databinding.FragmentHomeBinding
+import com.example.noteapp.viewmodel.NoteViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class HomeFragment : Fragment(R.layout.fragment_home) , SearchView.OnQueryTextListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding  : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        // setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -35,26 +31,25 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        notesViewModel = (activity as MainActivity).notesViewModel
+
+        setUpRecyclerView()
+
+        binding.fabAddNote.setOnClickListener {
+            it.findNavController().navigate(
+                R.id.action_homeFragment_to_newNoteFragment
+            )
+        }
     }
+
+    private fun setUpRecyclerView() {
+        TODO("Not yet implemented")
+    }
+
 }
