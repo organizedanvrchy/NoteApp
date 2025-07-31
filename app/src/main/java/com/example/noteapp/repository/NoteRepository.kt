49ada98c8765a5f839aa748cf0e263b22dc564e3
organizedanvrchy <vimalramnarain@gmail.com>
2025-07-30 20:59:36 -0400

    Add Repository and ViewModel files

---
+++ app/src/main/java/com/example/noteapp/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.noteapp.repository
+
+import com.example.noteapp.database.NoteDatabase
+import com.example.noteapp.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+
+    suspend fun insertNote(note: Note) = db.getNoteDAO().insertNote(note)
+    suspend fun deleteNote(note: Note) = db.getNoteDAO().deleteNote(note)
+    suspend fun updateNote(note: Note) = db.getNoteDAO().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDAO().getAllNotes()
+    fun searchNote(query: String?) = db.getNoteDAO().searchNote(query)
+
+}
